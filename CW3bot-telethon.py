16f54323fb350d53748d3edc7f5bda89626f091d
AndreyMarunyak <and.marunyak@gmail.com>
2018-09-28 17:59:29 +0300

    initialize get_hero_message function

--- CW3bot-telethon.py
+++ CW3bot-telethon.py
@@ -3,9 +3,19 @@ import random
 from time import time, sleep
 from telethon import TelegramClient, events
 
-api_hash = ''
+api_hash = 'bb85650739037a67603d57146707722a'
 
-api_id = 
+api_id = 409382
+
+game_id = 265204902
+
+client = TelegramClient('CW3bot', api_id, api_hash)
+client.start()
+
+@client.on(events.NewMessage(from_users = game_id, pattern = r'Битва семи замков через'))
+async def get_message_hero(event):
+	print('Received message from bot')
 
 
-client = TelegramClient = ('CW3bot', api_id, api_hash)
+if __name__ == '__main__':
+	client.run_until_disconnected()
